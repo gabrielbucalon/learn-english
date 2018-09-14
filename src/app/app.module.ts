84e64655914c813d2cf05cafdfc1c210ef6508ef
Gabriel <bucalongabriell@gmail.com>
2018-09-14 15:40:25 -0300

    Feature - Aprendendo inglês

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PainelComponent } from './painel/painel.component';
+import { TopoComponent } from './topo/topo.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProcessoComponent } from './processo/processo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelComponent,
+    TopoComponent,
+    TentativasComponent,
+    ProcessoComponent
   ],
   imports: [
     BrowserModule
