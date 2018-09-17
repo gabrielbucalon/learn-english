daa6f245f2153df4a8456051e69b9885dfc277ce
Gabriel <bucalongabriell@gmail.com>
2018-09-17 15:05:40 -0300

    Feature - Alterações na barra de progresso

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,7 +11,8 @@ export class PainelComponent implements OnInit {
   public frase: Frase[] = FRASES;
   public resposta: string;
   rodada = 0;
-  public  rodadaFrase: Frase;
+  public rodadaFrase: Frase;
+  public progresso = 0;
 
   constructor() {
     this.rodadaFrase = this.frase[this.rodada];
@@ -26,10 +27,19 @@ export class PainelComponent implements OnInit {
   }
 
   verificaResposta(): void {
-    // console.log('Verifica resposta: ', this.resposta);
-    this.rodada++;
-    console.log(this.rodada);
-    this.rodadaFrase = this.frase[this.rodada];
-    console.log(this.rodadaFrase);
+    if (this.rodadaFrase.frasePt === this.resposta) {
+      alert('A tradução está correta!');
+      // console.log('Verifica resposta: ', this.resposta);
+      this.rodada++;
+
+      this.progresso += (100 / this.frase.length);
+      console.log(this.progresso);
+      console.log(this.rodada);
+      this.rodadaFrase = this.frase[this.rodada];
+      console.log(this.rodadaFrase);
+
+    } else {
+      alert('A tradução está incorreta!');
+    }
   }
 }
--- src/app/processo/processo.component.ts
+++ src/app/processo/processo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-processo',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./processo.component.css']
 })
 export class ProcessoComponent implements OnInit {
-
+  @Input() public progresso = 0;
   constructor() { }
 
   ngOnInit() {
